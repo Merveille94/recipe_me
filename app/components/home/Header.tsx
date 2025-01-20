359e9a4927e164eded7e85492dd06be39019a7e3
Merveille94 <emanueleguehi@gmail.com>
2025-01-20 04:00:05 +0000

    Header Page done with authentication

--- app/components/home/Header.tsx
+++ app/components/home/Header.tsx
@@ -4,22 +4,32 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className="container mx-auto flex items-center justify-between bg-black text-white p-5 font-semibold">
-            <Link href="/"><h1>Recipe & Me</h1></Link>
-            <nav>
-                <ul>
-                    <li><Link href={"/pages/user"}>Dashboard</Link></li>
-                    <li><Link href={"/pages/user"}>About</Link></li>
-                    <li><Link href={"/pages/user"}>Contacts</Link></li>
-                </ul>
-            </nav>
-            <div>
-            <SignedOut>
-                    <SignInButton/>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton/>
-                </SignedIn>
+        <header className="flex items-center justify-center bg-black text-white p-5 font-semibold">
+            <div className="container mx-auto flex items-center justify-between">
+                <div className="text-xl"><Link href="/"><h1>Recipe & Me</h1></Link></div>
+                <nav>
+                    <ul className="flex space-x-1 ">
+                        <li className="rounded-lg flex-1 p-2 bg-green-900 hover:bg-green-800 text-center">
+                            <Link href={"/pages/user"}>Dashboard</Link>
+                        </li>
+                        <li className="rounded-lg flex-1 p-2 px-4 bg-green-900 hover:bg-green-800 text-center">
+                            <Link href={"/pages/about"}>About</Link>
+                        </li>
+                        <li className="rounded-lg flex-1 p-2 bg-green-900 hover:bg-green-800 text-center">
+                            <Link href={"/pages/contact"}>Contacts</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+
+                <div>
+                    <SignedOut>
+                        <SignInButton/>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton/>
+                    </SignedIn>
+                </div>
             </div>
         </header>
     )
---
+++ app/pages/contact/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>ContactPage</div>
+    )
+}
+export default Page
